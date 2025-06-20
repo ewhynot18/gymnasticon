51bdbaa38ab8bb2ed2c464d8180a625f82818fe9
Eli Whynot <eliwhynot@elis-mbp.home>
2025-06-20 08:13:49 -0300

    enable debug

--- src/bikes/ic4.js
+++ src/bikes/ic4.js
@@ -180,6 +180,12 @@ export function parse(data) {
     const power = data.readInt16LE(IBD_VALUE_IDX_POWER);
     const cadence = Math.round(data.readUInt16LE(IBD_VALUE_IDX_CADENCE) / 2);
     const speed = data.readUInt16LE(IBD_VALUE_IDX_SPEED) / 100;
+    debuglog('PARSE DEBUG:', {
+      raw: data.toString('hex'),
+      power,
+      cadence,
+      speed,
+    })
     return {power, cadence, speed};
   }
   throw new Error('unable to parse message');
